0d28ac75adfbf852c89e8a743ae1dc3bcf33534b
Murilo Macedo <murilomacedosilva2@gmail.com>
2026-01-12 11:51:34 -0300

    alterations

---
+++ src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
@@ -0,0 +1,21 @@
+package br.com.advocaciaviviane.excecoes;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception e) {
+        e.printStackTrace();
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "Content-Type,Authorization")
+                .header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+                .entity("Erro interno na API")
+                .build();
+    }
+}
