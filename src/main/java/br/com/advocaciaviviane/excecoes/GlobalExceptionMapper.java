ed9d9961d9db46b4cb3043619636ceae8422dd93
Murilo Macedo <murilomacedosilva2@gmail.com>
2026-01-12 21:59:56 -0300

    mmm

--- src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
+++ src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
@@ -1,26 +1,26 @@
-package br.com.advocaciaviviane.excecoes;
-
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.ext.Provider;
-
-@Provider
-public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
-
-    @Override
-    public Response toResponse(Exception e) {
-        e.printStackTrace();
-
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-            .entity(new ErrorResponse("Erro interno na API"))
-            .build();
-    }
-
-    public static class ErrorResponse {
-        public String message;
-
-        public ErrorResponse(String message) {
-            this.message = message;
-        }
-    }
-}
+//package br.com.advocaciaviviane.excecoes;
+//
+//import jakarta.ws.rs.core.Response;
+//import jakarta.ws.rs.ext.ExceptionMapper;
+//import jakarta.ws.rs.ext.Provider;
+//
+//@Provider
+//public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+//
+//    @Override
+//    public Response toResponse(Exception e) {
+//        e.printStackTrace();
+//
+//        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+//            .entity(new ErrorResponse("Erro interno na API"))
+//            .build();
+//    }
+//
+//    public static class ErrorResponse {
+//        public String message;
+//
+//        public ErrorResponse(String message) {
+//            this.message = message;
+//        }
+//    }
+//}
