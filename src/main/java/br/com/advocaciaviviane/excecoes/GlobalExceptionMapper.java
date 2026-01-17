3f752b660bfa8fbcabf77497de7c941d1b6483f0
MuriloMacedoSilva <murilomacedosilva3@gmail.com>
2026-01-17 16:35:05 -0300

    commi

--- src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
+++ src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
@@ -1,25 +1,29 @@
-//package br.com.advocaciaviviane.excecoes;
-//
-//import jakarta.ws.rs.core.Response;
-//import jakarta.ws.rs.ext.ExceptionMapper;
-//import jakarta.ws.rs.ext.Provider;
-//
-//@Provider
-//public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
-//    @Override
-//    public Response toResponse(Exception e) {
-//        e.printStackTrace();
-//        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-//                // Forçamos os headers aqui para o erro aparecer no console do navegador
-//                .header("Access-Control-Allow-Origin", "*")
-//                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-//                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-//                .entity(new ErrorResponse("Erro detalhado: " + e.getMessage()))
-//                .build();
-//    }
-//
-//    public static class ErrorResponse {
-//        public String message;
-//        public ErrorResponse(String message) { this.message = message; }
-//    }
-//}
+package br.com.advocaciaviviane.excecoes;
+
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Context
+    HttpHeaders headers;
+
+    @Override
+    public Response toResponse(Exception e) {
+
+        // NÃO intercepta preflight
+        if ("OPTIONS".equalsIgnoreCase(headers.getRequestHeaders()
+                .getFirst(":method"))) {
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(e.getMessage())
+                .build();
+    }
+}
+
