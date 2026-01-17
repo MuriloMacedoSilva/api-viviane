b4706db51dd42aed3aa302e5c0f19bd22388be3b
Murilo Macedo Silva <murilomacedosilva@192.168.15.42>
2026-01-17 13:15:04 -0300

    commitr

--- src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
+++ src/main/java/br/com/advocaciaviviane/excecoes/GlobalExceptionMapper.java
@@ -1,25 +1,25 @@
-package br.com.advocaciaviviane.excecoes;
-
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.ext.Provider;
-
-@Provider
-public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
-    @Override
-    public Response toResponse(Exception e) {
-        e.printStackTrace();
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                // Forçamos os headers aqui para o erro aparecer no console do navegador
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-                .entity(new ErrorResponse("Erro detalhado: " + e.getMessage()))
-                .build();
-    }
-
-    public static class ErrorResponse {
-        public String message;
-        public ErrorResponse(String message) { this.message = message; }
-    }
-}
+//package br.com.advocaciaviviane.excecoes;
+//
+//import jakarta.ws.rs.core.Response;
+//import jakarta.ws.rs.ext.ExceptionMapper;
+//import jakarta.ws.rs.ext.Provider;
+//
+//@Provider
+//public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+//    @Override
+//    public Response toResponse(Exception e) {
+//        e.printStackTrace();
+//        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+//                // Forçamos os headers aqui para o erro aparecer no console do navegador
+//                .header("Access-Control-Allow-Origin", "*")
+//                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+//                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+//                .entity(new ErrorResponse("Erro detalhado: " + e.getMessage()))
+//                .build();
+//    }
+//
+//    public static class ErrorResponse {
+//        public String message;
+//        public ErrorResponse(String message) { this.message = message; }
+//    }
+//}
